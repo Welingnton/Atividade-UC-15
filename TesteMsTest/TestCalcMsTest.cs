e1ddef37535f785b40737f3b90c11a6dc81ddef6
WELINGNTON <wellington902008@hotmail.com>
2022-10-12 11:08:33 -0300

    Teste

---
+++ TesteMsTest/TestCalcMsTest.cs
@@ -0,0 +1,36 @@
+using Calculadora;
+
+namespace TesteMsTest
+{
+    [TestClass]
+    public class TestCalcMsTest
+    {
+        [TestMethod]
+        public void TesteSomarDoisNumeros()
+        {
+            //Arange - Preparação
+            double pNum = 1;
+            double sNum = 1;
+            double rNum = 2;
+
+            //Act - Ação
+            var resultado = Calculadora.Somar(pNum, sNum);
+
+            //Assert - Verificação
+            Assert.AreEqual(rNum, resultado);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 3)]
+        [DataRow(2, 2, 4)]
+        [DataRow(2, 5, 7)]
+        public void TesteSomarDoisNumerosLista(double pNum, double sNum, double rNum) 
+        {
+            //Act
+            var resultado = Calculadora.Somar(pNum, sNum);
+
+            //Assert
+            Assert.AreEqual(rNum, resultado);
+        }
+    }
+}
---
+++ TesteXUnit/TesteCalcXUnit.cs
@@ -0,0 +1,36 @@
+using Calculadora;
+
+namespace TesteXUnit
+{
+    public class TesteCalcXUnit
+    {
+        [Fact]
+        public void SomarDoisNumeros()
+        {
+            //Arrange - Preparação
+            double pNum = 1;
+            double sNum = 2;
+            double rNum = 3;
+
+            //Act - Ação
+            var resultado = Calculadora.Somar(pNum, sNum);
+
+            //Assert - Verificação
+            Assert.Equal(rNum, resultado);
+
+        }
+
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(1, 2, 4)]
+        [InlineData(2, 3, 5)]
+        public void SomarDoisNumerosLista(double pNum, double sNum, double rNum) 
+        {
+            //Act
+            var resultado = Calculadora.Somar(pNum, sNum);
+
+            //Assert
+            Assert.Equal(rNum, resultado);
+        }
+    }
+}
